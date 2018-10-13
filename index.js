bcedb5188da1ee9b10350ed84c70aa615eaaea63
Filipe Degrazia <filipedegrazia98@gmail.com>
2018-10-13 16:15:35 -0300

    Add Viewer to library

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import RichText from './RichText';
 
+import { EditorWebView } from './Editor';
+import { ViewerWebView } from './Viewer';
+
 export default RichText;
+
+export { EditorWebView, ViewerWebView };
