eec5d68485e003979a0723ca583080d2717b88a7
Filipe Degrazia <filipedegrazia98@gmail.com>
2020-10-30 15:27:20 -0300

    Rebase project

--- index.js
+++
@@ -1,8 +0,0 @@
-import RichText from './RichText';
-
-import { EditorWebView } from './Editor';
-import { ViewerWebView } from './Viewer';
-
-export default RichText;
-
-export { EditorWebView, ViewerWebView };
