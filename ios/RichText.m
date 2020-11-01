06336ad20630ed19fffd1e5f4c04857833c5939e
Filipe Degrazia <filipedegrazia98@gmail.com>
2020-10-31 23:26:46 -0300

    Set value through React Native

--- ios/RichText.m
+++ ios/RichText.m
@@ -2,4 +2,7 @@
 #import "React/RCTViewManager.h"
 
 @interface RCT_EXTERN_MODULE(RNRTEditor, NSObject)
+
+RCT_EXPORT_VIEW_PROPERTY(value, NSString)
+
 @end
--- ios/RichText.swift
+++ ios/RichText.swift
@@ -1,15 +1,34 @@
 import UIKit
 
 @objc(RNRTEditor)
-class RichTextEditor: RCTViewManager {
+class RichTextEditorManager: RCTViewManager {
   override func view() -> UIView! {
-    let textField = UITextField()
-    textField.text = "Enter your text"
-    textField.textAlignment = .center
-    return textField
+    return RichTextEditor()
   }
 
   override static func requiresMainQueueSetup() -> Bool {
     return true
+  }
+}
+
+class RichTextEditor: UIView {
+  var textField: UITextField!
+  @objc var value: NSString = "" {
+    didSet {
+      textField.text = String(value)
+    }
+  }
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    textField = UITextField()
+    textField.text = "Enter your text"
+    textField.textAlignment = .center
+    textField.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    self.addSubview(textField)
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
   }
 }
